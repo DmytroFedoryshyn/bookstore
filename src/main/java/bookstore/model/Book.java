da078d8889ab776e4247cfbc9fa2456e73e17d8b
WallRun <dimonty2003@gmail.com>
2023-08-04 21:57:01 +0300

    added book related classes

---
+++ src/main/java/bookstore/model/Book.java
@@ -0,0 +1,32 @@
+package bookstore.model;
+
+import java.math.BigDecimal;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private Long id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String author;
+    @NotNull
+    @Column(unique = true)
+    private String isbn;
+    @NotNull
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
