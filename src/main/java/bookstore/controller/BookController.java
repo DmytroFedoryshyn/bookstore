b81a41ae5086eea658aee4db3e36af78759ede11
WallRun <dimonty2003@gmail.com>
2023-08-12 14:38:38 +0300

    fixed style issues

--- src/main/java/bookstore/controller/BookController.java
+++ src/main/java/bookstore/controller/BookController.java
@@ -4,19 +4,29 @@ import bookstore.dto.BookDto;
 import bookstore.dto.BookSearchParametersDto;
 import bookstore.dto.CreateBookRequestDto;
 import bookstore.service.BookService;
-import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/books")
@@ -44,8 +54,10 @@ public class BookController {
     @Operation(summary = "Get a book by ID")
     @GetMapping("/{id}")
     @ApiResponse(responseCode = "200", description = "Book found", content = {
-            @Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class),
-                    examples = @ExampleObject(value = "{\"id\": 1, \"title\": \"Sample Book\", \"author\": \"John Doe\"}"))
+            @Content(mediaType = "application/json", schema
+                    = @Schema(implementation = BookDto.class),
+                    examples = @ExampleObject(value = "{\"id\": 1, \"title\":"
+                            + " \"Sample Book\", \"author\": \"John Doe\"}"))
     })
     @ApiResponse(responseCode = "404", description = "Book not found")
     public BookDto getById(@PathVariable Long id) {
@@ -56,8 +68,10 @@ public class BookController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @ApiResponse(responseCode = "201", description = "Book created", content = {
-            @Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class),
-                    examples = @ExampleObject(value = "{\"id\": 1, \"title\": \"Sample Book\", \"author\": \"John Doe\"}"))
+            @Content(mediaType = "application/json", schema =
+            @Schema(implementation = BookDto.class),
+                    examples = @ExampleObject(value = "{\"id\": 1, \"title\":"
+                            + " \"Sample Book\", \"author\": \"John Doe\"}"))
     })
     public BookDto create(@Valid @RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
@@ -66,8 +80,10 @@ public class BookController {
     @Operation(summary = "Update an existing book by ID")
     @PutMapping("/{id}")
     @ApiResponse(responseCode = "200", description = "Book updated", content = {
-            @Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class),
-                    examples = @ExampleObject(value = "{\"id\": 1, \"title\": \"Updated Book\", \"author\": \"Jane Smith\"}"))
+            @Content(mediaType = "application/json", schema =
+            @Schema(implementation = BookDto.class),
+                    examples = @ExampleObject(value = "{\"id\": 1,"
+                            + " \"title\": \"Updated Book\", \"author\": \"Jane Smith\"}"))
     })
     @ApiResponse(responseCode = "404", description = "Book not found")
     public BookDto update(@Valid @RequestBody CreateBookRequestDto bookDto, @PathVariable Long id) {
@@ -86,8 +102,10 @@ public class BookController {
     @Operation(summary = "Search for books by parameters")
     @GetMapping("/search")
     @ApiResponse(responseCode = "200", description = "Books found", content = {
-            @Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class),
-                    examples = @ExampleObject(value = "[{\"id\": 1, \"title\": \"Book 1\", \"author\": \"Author 1\"}]"))
+            @Content(mediaType = "application/json", schema =
+            @Schema(implementation = BookDto.class),
+                    examples = @ExampleObject(value = "[{\"id\": 1,"
+                            + " \"title\": \"Book 1\", \"author\": \"Author 1\"}]"))
     })
     public List<BookDto> searchBooks(
             @Parameter(description = "Search parameters for filtering books")
